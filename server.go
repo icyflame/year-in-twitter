985b727c35debac0de73995d7fbb625cf6bacacb
Siddharth Kannan <kannan.siddharth12@gmail.com>
2017-12-15 11:57:26 +0530

    implement pagination, wordcount, max RT and max Fav tweets

--- server.go
+++ server.go
@@ -4,7 +4,6 @@ import "github.com/joho/godotenv"
 
 import "github.com/dghubble/go-twitter/twitter"
 import "golang.org/x/oauth2"
-// import "github.com/dghubble/oauth1"
 
 import b64 "encoding/base64"
 import "encoding/json"
@@ -16,12 +15,28 @@ import "log"
 import "io/ioutil"
 import "strings"
 
+import "time"
+
 type BearerToken struct {
     Token_Type string
     Access_Token string
 }
 
+/*
+* 1. Number of words in total
+* 2. Most liked tweet
+* 3. Most retweeted tweet
+* 4. Total number of tweets
+*
+* Store in JSON and then regenerate only on request.
+*/
+
 func main() {
+
+    // Ref time: Mon Jan 2 15:04:05 MST 2006
+    begin, _ := time.Parse("2006-01-02", "2017-01-01")
+    // begin, _ := time.Parse("2006-01-02", "2017-12-01")
+
     err := godotenv.Load()
     if err != nil {
         log.Fatal("Error loading .env file")
@@ -95,19 +110,90 @@ func main() {
             r.ParseForm()
 
             log.Printf("POST req recd")
-            log.Print(r.PostForm)
 
             handle := r.PostForm.Get("handle")
-            fmt.Fprintf(w, "Hello, %s", handle)
 
             incRT := false
 
-            userTimelineParams := &twitter.UserTimelineParams{ScreenName: handle, Count: 2, IncludeRetweets: &incRT}
-            tweets, resp, err := client.Timelines.UserTimeline(userTimelineParams)
-            log.Printf("USER TIMELINE: %+v", tweets)
-            log.Printf("RESP: %+v", resp);
-            log.Print("Error:");
-            log.Printf("%+v", err);
+            num_tweets := 0
+            word_count := 0
+
+            var maxRT twitter.Tweet;
+            var maxFav twitter.Tweet;
+            firstRun := true
+
+            last_tw_time := time.Now()
+            var last_tw_id int64
+            last_tw_id = 0
+            var first_tw_in_period twitter.Tweet;
+
+            log.Printf("Searching for all tweets before %s", begin)
+
+            for last_tw_time.After(begin) {
+
+                userTimelineParams := &twitter.UserTimelineParams{ScreenName: handle,Count: 200,IncludeRetweets: &incRT}
+
+                if (last_tw_id > 0) {
+                    userTimelineParams.MaxID = last_tw_id;
+                }
+
+                tweets, _, err := client.Timelines.UserTimeline(userTimelineParams)
+
+                if err != nil {
+                    log.Fatal(err);
+                } else if len(tweets) > 0 {
+
+                    last_tw := tweets[len(tweets)-1]
+
+                    last_tw_time, _ = time.Parse(time.RubyDate, last_tw.CreatedAt)
+                    last_tw_id = last_tw.ID
+
+                    log.Printf("Recd %d tweets; uptill %s (From ID: %d, To ID: %d); ", len(tweets), last_tw_time, tweets[0].ID, last_tw_id)
+
+                    whole_set := last_tw_time.After(begin);
+
+                    if (whole_set) {
+                        num_tweets += len(tweets)
+                        first_tw_in_period = last_tw;
+                    }
+
+                    if firstRun {
+                        maxRT = tweets[0]
+                        maxFav = tweets[0]
+                        firstRun = false
+                    }
+
+                    for _, tweet := range tweets {
+                        this_tw_time, _ := time.Parse(time.RubyDate, tweet.CreatedAt)
+                        if !whole_set && this_tw_time.Before(begin) {
+                            break;
+                        }
+
+                        if (!whole_set) {
+                            num_tweets++;
+                            first_tw_in_period = tweet;
+                        }
+
+                        word_count += len(strings.Fields(tweet.Text))
+
+                        if (tweet.RetweetCount > maxRT.RetweetCount) {
+                            maxRT = tweet
+                        }
+
+                        if (tweet.FavoriteCount > maxFav.FavoriteCount) {
+                            maxFav = tweet
+                        }
+                    }
+                }
+
+                log.Printf("Word count: %d", word_count)
+                log.Printf("Number of tweets: %d", num_tweets)
+                log.Printf("First tweet in this period: %d", first_tw_in_period.ID)
+                log.Printf("Tweet with maximum favourites: %d", maxFav.ID)
+                log.Printf("Tweet with maximum Retweets: %d", maxRT.ID)
+            }
+
+            fmt.Fprintf(w, "Hello, %s. You wrote %d tweets this year, with a total of %d words!", handle, num_tweets, word_count)
         } else {
             fmt.Fprintf(w, "Unrecognized method. Only GET / and POST / are supported!");
         }
