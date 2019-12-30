144f7fb0fb4ed9bed3b66f6db20241ea296a68dc
Siddharth Kannan <mail@siddharthkannan.in>
2019-12-30 19:52:13 +0530

    Add a utils function to convert to readable number

---
+++ utils/utils_test.go
@@ -0,0 +1,34 @@
+package utils
+
+import (
+	"testing"
+
+	"github.com/kylelemons/godebug/pretty"
+)
+
+func TestHumanNumber(t *testing.T) {
+	type testCase struct {
+		input      int
+		wantOutput string
+	}
+
+	testCases := map[string]testCase{
+		"Base case": {
+			input:      4000,
+			wantOutput: "4,000",
+		},
+		"Base case 2": {
+			input:      400,
+			wantOutput: "400",
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			got := HumanNumber(tc.input)
+			if diff := pretty.Compare(got, tc.wantOutput); diff != "" {
+				t.Errorf("Unexpected result: (-got +want): %s", diff)
+			}
+		})
+	}
+}
