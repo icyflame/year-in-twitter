fc61dac8e2306e73f99119ee3a5f3928fea172d1
Siddharth Kannan <mail@siddharthkannan.in>
2019-12-30 19:57:31 +0530

    Remove un-used username from basic users script

--- basics.sh
+++ basics.sh
@@ -1,4 +1,4 @@
-users=(Casey icyflame ghostwriternr beenharsh SemiColonComma realdonaldtrump kendalljenner taylorswift13 lovebillynyc AyushPandey9 kritarthjha _icyflame HilaKleinH3 h3h3productions)
+users=(Casey _icyflame ghostwriternr beenharsh SemiColonComma realdonaldtrump kendalljenner taylorswift13 lovebillynyc AyushPandey9 kritarthjha HilaKleinH3 h3h3productions)
 for i in ${users[*]}; do
     echo "Getting $i"
     /usr/bin/time -f "%E" curl -s "https://twitter.siddharthkannan.in/get/$i" > /dev/null
