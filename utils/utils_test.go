aca58a1712db30d656f8a7fb7bb76255f4b55c48
Siddharth Kannan <mail@siddharthkannan.in>
2019-12-30 19:54:54 +0530

    Add one more test case

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -21,13 +21,17 @@ func TestHumanNumber(t *testing.T) {
 			input:      400,
 			wantOutput: "400",
 		},
+		"Base case 3": {
+			input:      1234568,
+			wantOutput: "1,234,568",
+		},
 	}
 
 	for name, tc := range testCases {
 		t.Run(name, func(t *testing.T) {
 			got := HumanNumber(tc.input)
 			if diff := pretty.Compare(got, tc.wantOutput); diff != "" {
-				t.Errorf("Unexpected result: (-got +want): %s", diff)
+				t.Errorf("Unexpected result: (-got +want):\n%s", diff)
 			}
 		})
 	}
